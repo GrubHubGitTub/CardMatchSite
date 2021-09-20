d6aee14b8838a3666a3b0e9d875ab14be8765a18
Aidan Gruber <aidangruber@hotmail.com>
2021-09-20 14:25:35 +0700

    changed homepage forms

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 import random
+import requests
 
 app = Flask(__name__)
 
@@ -10,20 +11,28 @@ def form():
 @app.route('/game', methods=["GET", "POST"])
 def game():
     if request.method == "POST":
-        input_is_url_list = str(request.form.getlist("imagecheck"))
-        print(input_is_url_list)
-        cards = {}
-        for n in range(1, 13):
-            text = request.form[f"Card{n}"]
-            if text == "":
+        cards = request.form["createcards"].splitlines()
+        print(cards)
+        card_dic = {}
+        count = 1
+        for card in cards[0:12]:
+            print(card)
+            if card == "":
                 pass
-            elif str(n) in input_is_url_list:
-                cards[f"url{n}"] = text
             else:
-                cards[str(n)] = text
-        l = list(cards.items())
+                try:
+                    r = requests.get(card)
+                    print(r.status_code)
+                    if r.status_code == 200:
+                        card_dic[f"url{count}"] = card
+                except Exception:
+                    card_dic[str(count)] = card
+                count += 1
+        print(card_dic)
+        l = list(card_dic.items())
         random.shuffle(l)
         shuffled_cards = dict(l)
+        print(shuffled_cards)
         return render_template("game.html", cards=shuffled_cards)
 
 if __name__ == "__main__":
